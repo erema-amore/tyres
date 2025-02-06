39b6d66fcf0adeda05bb475575f5f048f3daccea
Durstep <durstepblop@gmail.com>
2025-02-06 17:33:32 +0600

    '3'

---
+++ src/pages/admin.nur/Admin.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Admin = () => {
+
+
+  return (
+    <div>
+        <h2 className='txt'>АДМИНКА</h2>
+        <div className='box'>Добавить тавар</div>
+        <div className='box'>Добавить новасть</div>
+        <div className='box'>Заявки</div>
+        <div className='box'>Акции</div>
+    </div>
+  )
+}
+
+export default Admin
