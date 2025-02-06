7a56f5f5586dda4aba1b9549fc1affd99c5364dc
KairatJakshylykov01 <kairat2001jakshylykov@gmail.com>
2025-02-06 11:32:07 +0600

    create component Registration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Registration from './pages/registration/Registration';
 
 function App() {
   return (
     <div className="App">
-      
+      <Registration/>
     </div>
   );
 }
